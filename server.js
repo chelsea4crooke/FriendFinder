238e7d8bde7bf683ae12ff5ed5349026deb0d2c4
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-06 17:17:10 -0500

    still dont know

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require("express");
-
 var app = express();
+var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extend: true }));
 
-require("./app/routing/ApiRoutes");
-require("./app/routing/htmlRoutes");
+app.use(bodyParser.json({ type: "application/*+json" }));
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+app.use(bodyParser.text({ type: "text/html "}));
 
-app.listen(PORT, function(){
-    console.log("App listening on PORT: "+ PORT);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
