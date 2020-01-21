9c6f325a84ad6371ec66057222a1ccf88fb601c6
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-21 14:51:30 -0500

    still have cors policy block

--- app/routing/ApiRoutes.js
+++ app/routing/ApiRoutes.js
@@ -5,36 +5,56 @@ module.exports = function(app) {
     res.json(friends);
   });
   app.post("/api/friends", function(req, res) {
- 
+    var totalDifference = 0;
     var bestMatch = {
       name: " ",
       photo: " ",
       friendDifference: 1000
     };
-    console.log(req.body);
 
     var userData = req.body;
+    var userName = userData.name;
     var userScores = userData.scores;
-    
-    console.log(userScores);
 
-    var totalDifference = 0;
+    var b = userScores.map(function(item){
+      return parseInt(item, 10);
+    });
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: b
+    };
+
+    console.log("Name" + userName);
+    console.log("User Scores" + userScores);
+
+    var sum = b.reduce((a, b ) => a+ b, 0);
+    console.log("some of users scores " + sum);
+    console.log("Best match friend difference " + bestMatch.friendDifference);
+    console.log("=========================");
 
     for (var i = 0; i < friends.length; i++) {
-      console.log(friends[i]);
+      console.log(friends[i].name);
       totalDifference = 0;
+      console.log("total difference " + totalDifference);
+      console.log("best match friend difference " + bestMatch.friendDifference);
 
-      for (var j = 0; j < friends[i].scores[j]; j++) {
-        totalDifference += Math.abs(parseInt(userScore[j]) - parseInt(friends[i].scores[j]));
-      
+      var bfriendScore = friends[i].scores.reduce((a ,b) => a+ b, 0);
+      console.log("total friend score " + bfriendScore);
+      totalDifference += Math.abs(sum - bfriendScore);
+      console.log("------------------> " + totalDifference);
+
       if (totalDifference <= bestMatch.friendDifference) {
         bestMatch.name = friends[i].name;
         bestMatch.photo = friends[i].photo;
         bestMatch.friendDifference = totalDifference;
       }
+      console.log(totalDifference + "total difference");
     }
-  }
+    console.log(bestMatch);
     friends.push(userData);
+    console.log("new user added");
+    console.log(userData);
     res.json(bestMatch);
   });
 };
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extend: true }));
 
-app.use(bodyParser.json({ type: "application/*+json" }));
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html "}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.text());
+
+app.use(express.static("app/public"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
