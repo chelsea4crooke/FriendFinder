9bc9e25b654ab8d8b4327bea43bbdc72923af0e6
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-21 18:22:50 -0500

    files for heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
-var app = express();
 var bodyParser = require("body-parser");
 var path = require("path");
 
-var PORT = process.env.PORT || 8080;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extend: true }));
